cce02550020ae02513bbafcdde468ed04ddf26d3
Marc Selwan <marc@marcinthecloud.com>
2020-04-16 13:27:54 -0700

    fixed log grep

--- sai-demo/sai-demo/background.sh
+++ sai-demo/sai-demo/background.sh
@@ -4,7 +4,7 @@ wget https://downloads.datastax.com/enterprise/dse-6.8.tar.gz
 tar xzf dse-6.8.tar.gz
 sed -i 's/^cluster_name: .*$/cluster_name: "CQL Quickstart"/g' dse-6.8.0/resources/cassandra/conf/cassandra.yaml
 dse-6.8.0/bin/dse cassandra -R
-while [ `grep "Starting listening for CQL clients" dse-6.8/logs/system.log | wc -l` -lt 1 ]; do
+while [ `grep "Starting listening for CQL clients" /var/log/cassandra/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
 echo "done" >> /opt/katacoda-background-finished
